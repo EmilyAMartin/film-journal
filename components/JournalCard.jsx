77a7408d2a3d8819e10db2b405f4a24b94d4ef4f
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-12-24 14:14:18 -0330

    Adding journal page

---
+++ components/JournalCard.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
+
+const JournalCard = () => {
+	return (
+		<div>
+			<Card sx={{ maxWidth: 350 }}>
+				<CardActionArea>
+					<CardMedia
+						component='img'
+						height='500'
+						image='/src/Images/1.jpg'
+						alt='film poster'
+					/>
+
+					<CardActions disableSpacing></CardActions>
+					<CardContent>
+						<Typography
+							gutterBottom
+							variant='h5'
+							component='div'
+						>
+							Jounal Title
+						</Typography>
+						<Typography
+							variant='body2'
+							sx={{ color: 'text.secondary' }}
+						>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas soluta
+							rem, minus quos quo aspernatur ducimus laborum nemo adipisci, libero
+							necessitatibus cupiditate placeat eligendi culpa corporis non aliquid
+							pariatur? Similique.Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit. Quas soluta rem, minus quos quo aspernatur ducimus laborum nemo
+							adipisci, libero necessitatibus cupiditate placeat eligendi culpa
+							corporis non aliquid pariatur? Similique.Lorem ipsum dolor sit amet,
+							consectetur adipisicing elit. Quas soluta rem, minus quos quo aspernatur
+							ducimus laborum nemo adipisci, libero necessitatibus cupiditate placeat
+							eligendi culpa corporis non aliquid pariatur? Similique.
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+			</Card>
+		</div>
+	);
+};
+
+export default JournalCard;
--- pages/journal.jsx
+++ pages/journal.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import JournalCard from '../Components/JournalCard';
+import { Grid2 } from '@mui/material';
 
 const Journal = () => {
-	return <div>Journal</div>;
+	return (
+		<div>
+			<Grid2
+				container
+				spacing={3}
+				columns={16}
+				display={'flex'}
+				alignItems={'center'}
+				justifyContent={'center'}
+				marginTop={4}
+			>
+				<JournalCard />
+				<JournalCard />
+				<JournalCard />
+				<JournalCard />
+				<JournalCard />
+			</Grid2>
+		</div>
+	);
 };
 
 export default Journal;
